8ccbac7dce380872e73ac83fcbf2a59cd0c861c1
LPChaumont <lpchaumont3@gmail.com>
2022-11-06 14:09:55 -0500

    identify slow part of the code

--- code/Finding a Shared Motif.R
+++ code/Finding a Shared Motif.R
@@ -5,12 +5,10 @@ data <- readDNAStringSet("data/rosalind_lcsm.txt")
 lcs <- function(x){
   
   seq <- strsplit(toString(x), ", ")[[1]]
-  n <- length(seq)
   s <- seq[which.min(nchar(seq))][[1]]
   l <- nchar(s)
   allsubstring <- vector(mode = "list", length = l)
-  res <- c("")
-  
+
   # vector of all possible substrings of the shortest sequence
   for(i in 1:l){
     allsubstring[[i]] <- unique(substring(s, 1:(nchar(s) - i + 1), i:nchar(s)))
@@ -18,6 +16,7 @@ lcs <- function(x){
   allsubstring <- unlist(allsubstring)
   
   # check for the longest substring that is in all sequences
+  # I think that this is the slow part of my solution
   for(j in length(allsubstring):1){
     if(all(grepl(allsubstring[j], seq, fixed = TRUE))){
       res <- allsubstring[j]
